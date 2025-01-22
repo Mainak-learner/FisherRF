ced6d76abb439552f976e1fa1b66af443d842778
Mainak-learner <sarkar.mainak101@gmail.com>
2025-01-22 04:16:48 -0600

    fix import error

--- diff/cuda_rasterizer/rasterizer.h
+++ diff/cuda_rasterizer/rasterizer.h
@@ -50,6 +50,7 @@ namespace CudaRasterizer
 			const bool prefiltered,
 			float* out_color,
 			float* out_depth,
+			float* out_entropy,
 			int* radii = nullptr,
 			int* pixel_gaussian_counter=nullptr,
 			bool debug = false);
