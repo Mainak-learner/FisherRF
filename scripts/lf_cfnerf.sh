677913236b4c2ec287f2f1d9b792351ce6c15564
Mainak-learner <sarkar.mainak101@gmail.com>
2025-01-28 01:16:08 -0600

    change lf-exp bash script

--- scripts/lf_cfnerf.sh
+++ scripts/lf_cfnerf.sh
@@ -2,5 +2,5 @@ DATASET_PATH=$1
 EXP_PATH=$2
 OBJ=$3
 
-echo python active_train.py -s $DATASET_PATH/${OBJ}/corrected -m ${EXP_PATH} --method=H_reg --seed=0 --override_idxs ${OBJ} --eval --schema=v20seq1_inplace --eval --resolution 2 --iterations 15000 --test_iterations 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000 11000 12000 13000 14000 15000 --densify_until_iter=8000 --sh_up_every=1000 --sh_degree=2
-python active_train.py -s $DATASET_PATH/${OBJ}/corrected -m ${EXP_PATH} --method=H_reg --seed=0 --override_idxs ${OBJ} --eval --schema=v20seq1_inplace --eval --resolution 2 --iterations 15000 --test_iterations 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000 11000 12000 13000 14000 15000 --densify_until_iter=8000 --sh_up_every=1000 --sh_degree=2
+echo python active_train.py -s $DATASET_PATH/${OBJ}/corrected -m ${EXP_PATH} --method=H_reg --seed=0 --override_idxs ${OBJ} --eval --schema=v20seq1_inplace --eval --resolution 2 --iterations 40000 --test_iterations 4000 8000 12000 16000 20000 24000 28000 32000 36000 40000 --densify_until_iter=38000 --sh_up_every=1000 --sh_degree=2
+python active_train.py -s $DATASET_PATH/${OBJ}/corrected -m ${EXP_PATH} --method=H_reg --seed=0 --override_idxs ${OBJ} --eval --schema=v20seq1_inplace --eval --resolution 2 --iterations 40000 --test_iterations 4000 8000 12000 16000 20000 24000 28000 32000 36000 40000 --densify_until_iter=38000 --sh_up_every=1000 --sh_degree=2
