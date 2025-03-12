1550488eb237add5f47f59cb5d89a40166869830
Mainak-learner <sarkar.mainak101@gmail.com>
2025-03-12 18:09:21 -0500

    add import option for new primitive-based view selector class

--- active/__init__.py
+++ active/__init__.py
@@ -1,5 +1,6 @@
 from .rand_selector import RandSelector
 from .H_reg import HRegSelector
 from .entropy_selector import EntropySelector
+from .primitive_selector import PrimitiveSelector
 
-methods_dict = {"rand": RandSelector, "H_reg": HRegSelector, "entropy": EntropySelector}
+methods_dict = {"rand": RandSelector, "H_reg": HRegSelector, "entropy": EntropySelector, "uncertainty": PrimitiveSelector}
