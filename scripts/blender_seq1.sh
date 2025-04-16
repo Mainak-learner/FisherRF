6bfc1660e5e9715bfaf9a2b9841ea5a8caedb0d5
Mainak-learner <sarkar.mainak101@gmail.com>
2025-04-16 11:01:49 -0500

    change train script for blender dataset

--- scripts/blender_seq1.sh
+++ scripts/blender_seq1.sh
@@ -1,5 +1,5 @@
 DATASET_PATH=$1
 EXP_PATH=$2
 
-echo python active_train.py -s $DATASET_PATH -m ${EXP_PATH} --eval --method=var_uncertainty --seed=0 --schema v20seq1_inplace --iterations 10000  --white_background
-python active_train.py -s $DATASET_PATH -m ${EXP_PATH} --eval --method=var_uncertainty --seed=0 --schema v20seq1_inplace --iterations 10000  --white_background
+echo python active_train.py -s $DATASET_PATH -m ${EXP_PATH} --eval --method=var_uncertainty --seed=0 --schema v20seq1_inplace --densify_until_iter=20000 --iterations 40000  --white_background
+python active_train.py -s $DATASET_PATH -m ${EXP_PATH} --eval --method=var_uncertainty --seed=0 --schema v20seq1_inplace --densify_until_iter=20000 --iterations 40000  --white_background
