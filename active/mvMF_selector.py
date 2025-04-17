6436195285be3e4b9366433b516e7129d9f5ad75
Mainak-learner <sarkar.mainak101@gmail.com>
2025-04-17 09:47:33 -0500

    fix selector code

--- active/mvMF_selector.py
+++ active/mvMF_selector.py
@@ -16,7 +16,7 @@ def get_camera_centers(cameras):
     centers = []
     for cam in cameras:
         # Extract the camera-to-world matrix (assumes shape [4,4])
-        c2w = cam.world_view_transform.inverse().matrix  # [4,4]
+        c2w = cam.world_view_transform.inverse()  # [4,4]
         center = c2w[:3, 3]  # camera position in world coords
         centers.append(center.detach().cpu().numpy())
     return centers
