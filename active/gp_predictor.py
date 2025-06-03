fix gradient computation

active/gp_predictor.py
@@ -115,7 +115,6 @@ class VDGPFisherNBVSelector(Module):
         X_train = X_train.to(self.device)
         y_train = y_train.to(self.device)
 
-        # Consistency
         self.gp1.Xu = self.gp1.Xu.to(self.device)
         self.gp2.Xu = self.gp2.Xu.to(self.device)
         for param in self.gp1.kernel.parameters():
@@ -123,14 +122,14 @@ class VDGPFisherNBVSelector(Module):
         for param in self.gp2.kernel.parameters():
             param.data = param.data.to(self.device)
 
-        self.gp1.set_data(X=X_train)
-        h_mean, _ = self.gp1.forward(X_train)
-        h_latent = self.projection(h_mean.unsqueeze(-1))
+        with torch.no_grad():
+            self.gp1.set_data(X=X_train)
+            h_mean, _ = self.gp1.forward(X_train)
+            h_latent = self.projection(h_mean.unsqueeze(-1)).detach()
 
         self.gp2.set_data(X=h_latent, y=y_train)
         self.gp2.num_data = y_train.size(0)
 
-        # Pyro’s built-in SVI loop
         optimizer = pyro.optim.Adam({"lr": lr})
         elbo = pyro.infer.Trace_ELBO()
         svi = pyro.infer.SVI(self.gp2.model, self.gp2.guide, optimizer, elbo)
@@ -139,6 +138,7 @@ class VDGPFisherNBVSelector(Module):
             loss = svi.step()
             if (i+1) % 50 == 0:
                 print(f"Step {i+1}/{num_steps}, Loss: {loss:.3f}")
+
 
 
 