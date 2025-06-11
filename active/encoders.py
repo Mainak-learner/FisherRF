e33eacc57e7caaf14c1a5a874100ea2e221c5dcf
Mainak-learner <sarkar.mainak101@gmail.com>
2025-06-11 14:24:12 -0500

    use Fourier Encoding similar to NeRF

--- active/encoders.py
+++ active/encoders.py
@@ -41,3 +41,28 @@ class PoseToImageSIREN(nn.Module):
         x = torch.sin(self.omega_0 * self.fc1(x))
         x = torch.sin(self.omega_0 * self.fc2(x))
         return self.fc3(x)
+    
+class FourierEncoder(nn.Module):
+    def __init__(self, input_dim=3, num_frequencies=6):
+        super().__init__()
+        self.B = 2 ** torch.arange(num_frequencies).float() * 3.1415  # shape: [L]
+        
+    def forward(self, x):
+        x = x.unsqueeze(-1) * self.B.to(x.device)  # [B, 3, L]
+        return torch.cat([torch.sin(x), torch.cos(x)], dim=-1).view(x.shape[0], -1)  # [B, 3*2*L]
+
+class PoseToImageFFMLP(nn.Module):
+    def __init__(self, pose_dim=3, image_feat_dim=128, hidden_dim=256, num_frequencies=6):
+        super().__init__()
+        self.encoder = FourierEncoder(pose_dim, num_frequencies)
+        self.net = nn.Sequential(
+            nn.Linear(pose_dim * 2 * num_frequencies, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, image_feat_dim),
+        )
+
+    def forward(self, x):
+        x_encoded = self.encoder(x)
+        return self.net(x_encoded)
