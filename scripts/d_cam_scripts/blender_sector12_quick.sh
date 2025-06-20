5335158c45fb695948c794c7a52589fde6fbde25
Mainak-learner <sarkar.mainak101@gmail.com>
2025-06-19 22:59:34 -0500

    change initial train to 100 in script

--- scripts/d_cam_scripts/blender_sector12_quick.sh
+++ scripts/d_cam_scripts/blender_sector12_quick.sh
@@ -4,7 +4,7 @@ ORACLE_PATH=$3
 
 echo "Running sector-based active training..."
 
-CMD="python active_train_sector.py -s $DATASET_PATH -m $EXP_PATH --initial_train 10000 --oracle_model_path $ORACLE_PATH --iterations 30000 --white_background --deepkgp --test_iterations 5000 10000 15000 20000 25000 30000 --save_iterations 7000 30000"
+CMD="python active_train_sector.py -s $DATASET_PATH -m $EXP_PATH --initial_train 100 --oracle_model_path $ORACLE_PATH --iterations 30000 --white_background --deepkgp --test_iterations 5000 10000 15000 20000 25000 30000 --save_iterations 7000 30000"
 
 echo $CMD
 eval $CMD
