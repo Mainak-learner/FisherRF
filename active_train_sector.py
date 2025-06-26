b891d18f859b31f777d4fe5216a9d46294e7b6c1
Mainak-learner <sarkar.mainak101@gmail.com>
2025-06-26 09:18:07 -0500

    fix import issue

--- active_train_sector.py
+++ active_train_sector.py
@@ -25,6 +25,7 @@ import json
 from PIL import Image
 from active.mc_dkl_selector import MCDKLNBVSelector
 from active.gp_predictor import GPFisherNBVSelector, VDGPFisherNBVSelector
+from gaussian_renderer import render, network_gui, modified_render
 import torchvision.transforms.functional as TF
 from arguments import ModelParams, PipelineParams, OptimizationParams
 from active.train_phi_pose_to_feat import train_phi_sector
