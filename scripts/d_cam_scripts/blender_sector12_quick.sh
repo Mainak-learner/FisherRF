def8b5890c2d8b889ccc6f08ee8927f75cadc4c0
Mainak-learner <sarkar.mainak101@gmail.com>
2025-07-01 17:30:04 -0700

    change script

--- scripts/d_cam_scripts/blender_sector12_quick.sh
+++ scripts/d_cam_scripts/blender_sector12_quick.sh
@@ -5,7 +5,7 @@ PROCESS=$4
 
 echo "Running sector-based active training..."
 
-CMD="python active_train_sector.py -s $DATASET_PATH -m $EXP_PATH --initial_train 100 --oracle_model_path $ORACLE_PATH --iterations 10000 --white_background --nbv_process $PROCESS --deepkgp --test_iterations 5000 10000 15000 20000 25000 30000 --save_iterations 7000 30000"
+CMD="python active_train_sector.py -s $DATASET_PATH -m $EXP_PATH --initial_train 5000 --oracle_model_path $ORACLE_PATH --iterations 10000 --white_background --nbv_process $PROCESS --deepkgp --test_iterations 5000 10000 15000 20000 25000 30000 --save_iterations 7000 30000"
 
 echo $CMD
 eval $CMD
