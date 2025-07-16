a817cd9089f12843e7ae78ff8d18c40a3e7868ee
Mainak-learner <sarkar.mainak101@gmail.com>
2025-07-16 11:29:17 -0500

    include seed option in script

--- scripts/d_cam_scripts/blender_sector12_quick.sh
+++ scripts/d_cam_scripts/blender_sector12_quick.sh
@@ -2,10 +2,11 @@ DATASET_PATH=$1
 EXP_PATH=$2
 ORACLE_PATH=$3
 PROCESS=$4
+SEED=$5
 
 echo "Running sector-based active training..."
 
-CMD="python active_train_sector.py -s $DATASET_PATH -m $EXP_PATH --initial_train 5001 --oracle_model_path $ORACLE_PATH --iterations 10000 --white_background --nbv_process $PROCESS --deepkgp --test_iterations 5000 10000 15000 20000 25000 30000 --save_iterations 7000 30000"
+CMD="python active_train_sector.py -s $DATASET_PATH -m $EXP_PATH --initial_train 5001 --seed $SEED --oracle_model_path $ORACLE_PATH --iterations 10000 --white_background --nbv_process $PROCESS --deepkgp --test_iterations 5000 10000 15000 20000 25000 30000 --save_iterations 7000 30000"
 
 echo $CMD
 eval $CMD
