2ea79234a8a2145dc781f3897234124abf63f4c0
Mainak-learner <sarkar.mainak101@gmail.com>
2025-08-15 07:17:05 -0500

    change script for mipnerf scenes

---
+++ scripts/d_cam_scripts_mip/mip360_all.sh
@@ -0,0 +1,5 @@
+DATASET_PATH=$1
+EXP_PATH=$2
+
+echo python active_train.py -s $DATASET_PATH -m ${EXP_PATH} --eval --method=H_reg --seed=0 --schema all --iterations 50000
+python active_train.py -s $DATASET_PATH -m ${EXP_PATH} --eval --method=H_reg --seed=0 --schema all --iterations 50000
