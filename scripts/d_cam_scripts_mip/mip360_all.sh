c8f8e56f89598fb386cc3e97beca45649b64ca2c
Mainak-learner <sarkar.mainak101@gmail.com>
2025-08-20 08:12:51 -0500

    change script

--- scripts/d_cam_scripts_mip/mip360_all.sh
+++ scripts/d_cam_scripts_mip/mip360_all.sh
@@ -1,5 +1,5 @@
 DATASET_PATH=$1
 EXP_PATH=$2
 
-echo python active_train.py -s $DATASET_PATH -m ${EXP_PATH} -i images_4 --eval --method=H_reg --seed=0 --schema all --iterations 50000
-python active_train.py -s $DATASET_PATH -m ${EXP_PATH} -i images_4 --eval --method=H_reg --seed=0 --schema all --iterations 50000
+echo python active_train.py -s $DATASET_PATH -m ${EXP_PATH} -i images_8 --eval --method=H_reg --seed=0 --schema all --iterations 50000
+python active_train.py -s $DATASET_PATH -m ${EXP_PATH} -i images_8 --eval --method=H_reg --seed=0 --schema all --iterations 50000
--- scripts/d_cam_scripts_mip/mip360_blender12.sh
+++ scripts/d_cam_scripts_mip/mip360_blender12.sh
@@ -6,7 +6,7 @@ SEED=$5
 
 echo "Running sector-based active training..."
 
-CMD="python active_train_sector.py -i images_4 -s $DATASET_PATH -m $EXP_PATH --initial_train 5001 --seed $SEED --oracle_model_path $ORACLE_PATH --iterations 30000 --white_background --nbv_process $PROCESS --deepkgp --test_iterations 5000 10000 15000 20000 25000 30000 --save_iterations 7000 30000"
+CMD="python active_train_sector.py -i images_8 -s $DATASET_PATH -m $EXP_PATH --initial_train 5001 --seed $SEED --oracle_model_path $ORACLE_PATH --iterations 30000 --white_background --nbv_process $PROCESS --deepkgp --test_iterations 5000 10000 15000 20000 25000 30000 --save_iterations 7000 30000"
 
 echo $CMD
 eval $CMD
